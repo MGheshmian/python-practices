5f8861c76029388c8062c78199c947eb015c8473
MGheshmian <110479541+MGheshmian@users.noreply.github.com>
2023-04-12 15:50:47 +0330

    Delete numpy practice.py

--- numpy practice.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-r = np.ones(5).reshape(1, 5)
-a = np.concatenate([np.arange(0, 5).reshape(1, 5), np.array(np.arange(0, 5)[::-1]).reshape(1, 5)], axis=0)
-d = np.arange(1, 20, 2).reshape(2, 5)
-b = np.concatenate([r, a], axis=0)
-c = np.concatenate([b, d], axis=0)
-print(c)
